69c2a2ab288f91e5c4dd1daee0a33b93008931ec
Karolina Klisz <karolinaklisz1999@gmail.com>
2025-12-23 13:59:00 +0100

    add files: MoveDirection, OptionsParser, World

---
+++ projekt_java/src/main/java/Project/Model/MoveDirection.java
@@ -0,0 +1,8 @@
+package Project.Model;
+
+public enum MoveDirection {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+}
---
+++ projekt_java/src/main/java/Project/OptionsParser.java
@@ -0,0 +1,40 @@
+package Project;
+
+import Project.Model.MoveDirection;
+
+public class OptionsParser {
+    public static MoveDirection[] parser(String[] args){
+
+        int legalMoveCounter = 0;
+        for (String arg : args) {
+            if (arg.equals("f") || arg.equals("b") || arg.equals("l") || arg.equals("r"))
+                legalMoveCounter++;
+        }
+
+        MoveDirection[] directions = new MoveDirection[legalMoveCounter];
+        int i = 0;
+        for(String arg: args){
+            switch (arg){
+                case "f":
+                    directions[i] = MoveDirection.FORWARD;
+                    i++;
+                    break;
+                case "b":
+                    directions[i] = MoveDirection.BACKWARD;
+                    i++;
+                    break;
+                case "r":
+                    directions[i] = MoveDirection.RIGHT;
+                    i++;
+                    break;
+                case "l":
+                    directions[i] = MoveDirection.LEFT;
+                    i++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return directions;
+    }
+}
